7c16c5e472fc70e40a2516f8af082d1bd2a5220c
Dongho Sim <dhsim86@gmail.com>
2021-04-11 19:11:39 +0900

    Init

---
+++ src/main/java/Application.java
@@ -0,0 +1,11 @@
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Application {
+
+  public static void main(String[] args) {
+    log.info("test");
+  }
+
+}
